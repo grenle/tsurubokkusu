0d8091028da6d86b90881f7b9887d3907f5c9b6f
Gregory Houldsworth <gregory.houldsworth@protonmail.com>
2021-04-18 15:53:18 +0200

    small fix to random.pick, add jsdoc for random, no version change

--- src/random/index.js
+++ src/random/index.js
@@ -1,13 +1,36 @@
+//@ts-check
+
+/**
+ * Returns a random number between min (inclusive) and max
+ * (exclusive)
+ * @param {Number} min 
+ * @param {Number} max 
+ * @returns {Number}
+ */
 export function randRange(min, max){
     return Math.round(
         Math.random() * (max - min) + min
     )
 }
 
+
+/**
+ * Returns a random number between min (inclusive) and max
+ * (inclusive)
+ * @param {Number} min 
+ * @param {Number} max 
+ * @returns {Number}
+ */
 export function randInt(min, max){
     return randRange(min, max + 1)
 }
 
+
+/**
+ * Picks an random element from xs
+ * @param {Array} xs 
+ * @returns {*}
+ */
 export function pick(xs){
-    return items[randRange(0, xs.length)]
+    return xs[randRange(0, xs.length)]
 }
